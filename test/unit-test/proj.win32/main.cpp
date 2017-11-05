9ae6a1a8a2b8b9e353179a845c0fe4e992357bed
kevinwu1024 <clock4096@gmail.com>
2017-11-05 21:41:18 +0800

    added win32 projects

--- test/unit-test/proj.win32/main.cpp
+++ test/unit-test/proj.win32/main.cpp
@@ -1,6 +1,7 @@
 #include "main.h"
 #include "AppDelegate.h"
 #include "cocos2d.h"
+#include "gtest/gtest.h"
 
 USING_NS_CC;
 
@@ -13,6 +14,10 @@ int WINAPI _tWinMain(HINSTANCE hInstance,
     UNREFERENCED_PARAMETER(lpCmdLine);
 
     // create the application instance
-    AppDelegate app;
-    return Application::getInstance()->run();
+    //AppDelegate app;
+    //return Application::getInstance()->run();
+	int argc = 1;
+	char *argv[] = {"abc"};
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
 }
