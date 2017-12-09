dd4d4c4ec4d489a2fe9aabb15004c44e515e5843
Kevin Wu <clock4096@gmail.com>
2017-12-09 23:42:41 +0800

    makes nodes in common use to container

--- speedcc/cocos/SCUIMacro.h
+++ speedcc/cocos/SCUIMacro.h
@@ -63,22 +63,15 @@ namespace SpeedCC
 }
 
 
-#define SC_DEFINE_CONTAINER_VAR(_parent_) \
-    SCContainerEndFunctor* sc_container_pEndFunctor = NULL; \
-    cocos2d::Layer* sc_container_pParentNode = (_parent_); \
-    cocos2d::Vector<cocos2d::MenuItem*> sc_container_MenuItemArray;\
-    std::list<cocos2d::Ref*>    sc_container_LayoutObjectList;\
-    cocos2d::Menu* sc_container_pButtonListMenu = NULL;
+///-------------- root container
 
-
-// begin root container
 #define SC_BEGIN_CONTAINER_ROOT(_node_,_x_,_y_,_property_,_size_) \
     SC_BEGIN_CONTAINER_ROOT_EX((_node_),(_x_),(_y_),(_property_),(_size_),getRootLayer())
 
 
 #define SC_BEGIN_CONTAINER_ROOT_EX(_node_,_x_,_y_,_property_,_size_,_parent_)\
 {\
-    SC_DEFINE_CONTAINER_VAR(_parent_) \
+    ___SC_INSIDE_DEFINE_CONTAINER_VAR(_parent_) \
     int sc_root_nSCContainerStackCounter = 1;\
     sc_container_pParentNode->setContentSize(_size_);\
     sc_container_pParentNode->setAnchorPoint(cocos2d::Vec2(0.5,0.5));\
@@ -87,8 +80,9 @@ namespace SpeedCC
     if(temSize.width==0 || temSize.height==0) {temSize= sc_container_pParentNode->getContentSize();} \
     sc_container_pParentNode->setPosition(SCNodeUtils::posR2A(cocos2d::Vec2((_x_),(_y_)),temSize)) ; \
     assignNode(sc_container_pParentNode,(_node_));\
-    SCNodeProperty::setProperty<std::remove_pointer<decltype(sc_container_pParentNode)>::type>(sc_container_pParentNode,purifyProertyString((_property_)));\
+SCNodeProperty::setProperty<std::remove_pointer<decltype(sc_container_pParentNode)>::type>(sc_container_pParentNode,purifyProertyString((_property_)));\
     sc_container_LayoutObjectList.push_back(sc_container_pParentNode);
+
 
 // end container
 #define SC_END_CONTAINER \
@@ -107,21 +101,47 @@ namespace SpeedCC
 }
 
 
-// insert a sprite
+////------------- sprite related
+// insert sprite node
 #define SC_INSERT_SPRITE(_node_,_x_,_y_,_property_,_image_) \
     do{\
-        cocos2d::Sprite* pSCTemSprite = cocos2d::Sprite::create(SCFileUtils::getFullPathFile((_image_)).c_str());\
-        assignNode(pSCTemSprite,(_node_));\
-        sc_container_LayoutObjectList.push_back(pSCTemSprite);\
-        ___SC_INSIDE_ADD_LAYOUT_NODE(sc_container_pParentNode,pSCTemSprite,_x_,_y_)\
-        SCNodeProperty::setProperty<cocos2d::Sprite>(pSCTemSprite,purifyProertyString((_property_)));\
+        ___SC_INSIDE_ADD_SPRITE(_node_,_x_,_y_,_property_,_image_)\
     }while(0);
 
-// insert button with image
-#define SC_INSERT_BUTTON_IMAGE(_node_,_x_,_y_,_property_,_image_normal_,_image_select_,_image_disable_,_fun_)\
-    ___SC_INSIDE_ADD_BUTTON_IMAGE((_node_),(_x_),(_y_),(_property_),(_image_normal_),(_image_select_),(_image_disable_),_fun_)
+// sprite container
+#define SC_BEGIN_CONTAINER_SPRITE(_node_,_x_,_y_,_property_,_image_) \
+{\
+    cocos2d::Sprite* pSCContainerSprite = NULL;\
+    ___SC_INSIDE_ADD_SPRITE(pSCContainerSprite,_x_,_y_,_property_,_image_) \
+    assignNode(pSCContainerSprite,(_node_)); \
+    ___SC_INSIDE_DEFINE_CONTAINER_VAR(pSCContainerSprite)
 
-// insert customize's Node that user created
+///-------------- button related
+// insert image button
+#define SC_INSERT_BUTTON_IMAGE(_node_,_x_,_y_,_property_,_image_normal_,_image_select_,_image_disable_,_fun_)\
+    do{\
+        ___SC_INSIDE_ADD_BUTTON_IMAGE((_node_),(_x_),(_y_),(_property_),(_image_normal_),(_image_select_),(_image_disable_),_fun_) \
+    }while(0);
+
+// container image button
+#define SC_BEGIN_CONTAINER_BUTTON_IMAGE(_node_,_x_,_y_,_property_,_image_normal_,_image_select_,_image_disable_,_fun_) \
+{\
+    cocos2d::MenuItem* pSCContainerMenuItem = NULL;\
+    ___SC_INSIDE_ADD_BUTTON_IMAGE(pSCContainerMenuItem,(_x_),(_y_),(_property_),(_image_normal_),(_image_select_),(_image_disable_),_fun_) \
+    assignNode(pSCContainerMenuItem,(_node_)); \
+    ___SC_INSIDE_DEFINE_CONTAINER_VAR(pSCContainerMenuItem)
+
+
+// insert ttf button
+#define SC_INSERT_BUTTON_TTF(_node_,_x_,_y_,_property_,_string_,_font_,_size_,_fun_) \
+do{\
+    SCString strSCTemText = (_string_);\
+    auto pSCTemLabel = cocos2d::Label::createWithTTF(strSCTemText.c_str(),(_font_),(_size_));\
+    ___SC_INSIDE_ADD_BUTTON_LABEL((_node_),(_x_),(_y_),(_property_),pSCTemLabel,(_fun_))\
+    }while(0);
+
+///-------------- user node related
+// insert customize Node that user created
 #define SC_INSERT_USER_NODE(_node_,_x_,_y_,_property_) \
     do{\
         ___SC_INSIDE_ADD_LAYOUT_NODE(sc_container_pParentNode,(_node_),(_x_),(_y_));\
@@ -129,9 +149,39 @@ namespace SpeedCC
     }while(0);
 
 
+#define SC_BEGIN_CONTAINER_USER_NODE(_node_,_x_,_y_,_property_) \
+{\
+    ___SC_INSIDE_ADD_LAYOUT_NODE(sc_container_pParentNode,(_node_),(_x_),(_y_));\
+    SCNodeProperty::setProperty<std::remove_pointer<decltype(_node_)>::type>((_node_),purifyProertyString((_property_)));\
+    ___SC_INSIDE_DEFINE_CONTAINER_VAR((_node_))
 
-////---------------- inside macro
+///-------------- label related
+// insert a label
+#define SC_INSERT_LABEL_TTF(_node_,_x_,_y_,_property_,_string_,_font_,_size_)\
+    do{\
+        ___SC_INSIDE_ADD_LABEL_TTF((_node_),(_x_),(_y_),(_property_),(_string_),(_font_),(_size_))\
+    }while(0);
+
+// label container
+#define SC_BEGIN_CONTAINER_LABEL_TTF(_node_,_x_,_y_,_property_,_string_,_font_,_size_)\
+{\
+    cocos2d::Label* pSCContainerLabel = NULL; \
+    ___SC_INSIDE_ADD_LABEL_TTF(pSCContainerLabel,(_x_),(_y_),(_property_),(_string_),(_font_),(_size_)) \
+    assignNode(pSCContainerLabel,(_node_)); \
+    ___SC_INSIDE_DEFINE_CONTAINER_VAR(pSCContainerLabel)
+
+///--------------- layer related
+
+//////////////////------------------------- inside macros ---------------------------
 // below macros are used inside above macros, do not use in game
+
+// common
+#define ___SC_INSIDE_DEFINE_CONTAINER_VAR(_parent_) \
+    SCContainerEndFunctor* sc_container_pEndFunctor = NULL; \
+    cocos2d::Node* sc_container_pParentNode = (_parent_); \
+    cocos2d::Vector<cocos2d::MenuItem*> sc_container_MenuItemArray;\
+    std::list<cocos2d::Ref*>    sc_container_LayoutObjectList;\
+    cocos2d::Menu* sc_container_pButtonListMenu = NULL;
 
 #define ___SC_INSIDE_ADD_LAYOUT_NODE(_parent_,_node_,_x_,_y_) \
     cocos2d::Node* pSCTemParent = (_parent_);\
@@ -139,32 +189,65 @@ namespace SpeedCC
         const cocos2d::Size& tParentSizeTem = pSCTemParent->getContentSize();\
         pSCTemParent->addChild(_node_);\
         (_node_)->setPosition(SCNodeUtils::posR2A(cocos2d::Vec2((_x_),(_y_)),tParentSizeTem)); \
-    }\
+    }
 
+// sprite
+#define ___SC_INSIDE_ADD_SPRITE(_node_,_x_,_y_,_property_,_image_)\
+    cocos2d::Sprite* pSCTemSprite = cocos2d::Sprite::create(SCFileUtils::getFullPathFile((_image_)).c_str());\
+    assignNode(pSCTemSprite,(_node_));\
+    sc_container_LayoutObjectList.push_back(pSCTemSprite);\
+    ___SC_INSIDE_ADD_LAYOUT_NODE(sc_container_pParentNode,pSCTemSprite,_x_,_y_)\
+    SCNodeProperty::setProperty<cocos2d::Sprite>(pSCTemSprite,purifyProertyString((_property_)));
 
+// label
+#define ___SC_INSIDE_ADD_LABEL_TTF(_node_,_x_,_y_,_property_,_string_,_font_,_size_) \
+    SCString strSCTemText = (_string_);\
+    auto pSCTemLabel = cocos2d::Label::createWithTTF(strSCTemText.c_str(),(_font_),(_size_));\
+    ___SC_INSIDE_ADD_LAYOUT_NODE(sc_container_pParentNode,pSCTemLabel,(_x_),(_y_));\
+    assignNode(pSCTemLabel,(_node_));\
+    SCNodeProperty::setProperty<std::remove_pointer<decltype(pSCTemLabel)>::type>(pSCTemLabel,purifyProertyString((_property_)));\
+    sc_container_LayoutObjectList.push_back(pSCTemLabel);\
+
+// button
 #define ___SC_INSIDE_ADD_BUTTON_IMAGE(_node_,_x_,_y_,_property_,_image_normal_,_image_select_,_image_disable_,_fun_) \
-do{\
     cocos2d::Sprite* pSCTemSprite[3] = {NULL};\
     pSCTemSprite[0] = cocos2d::Sprite::create(SCFileUtils::getFullPathFile(_image_normal_).c_str());\
     pSCTemSprite[1] = cocos2d::Sprite::create(SCFileUtils::getFullPathFile(_image_select_).c_str());\
     pSCTemSprite[2] = cocos2d::Sprite::create(SCFileUtils::getFullPathFile(_image_disable_).c_str());\
     cocos2d::MenuItemSprite* pSCItemImage = cocos2d::MenuItemSprite::create(pSCTemSprite[0],pSCTemSprite[1],pSCTemSprite[2],\
-[this](cocos2d::Ref* pSender) { this->onSCMenuItemPressed(pSender);}) ; \
+[this](cocos2d::Ref* pSender) { this->onSCMenuItemPressed(pSender);}); \
     if(!sc_container_MenuItemArray.empty()){\
         sc_container_MenuItemArray.pushBack(pSCItemImage);\
     }else if(sc_container_pButtonListMenu!=NULL){\
         sc_container_pButtonListMenu->addChild(pSCItemImage);\
     }else{\
-        cocos2d::Menu* pMenu = cocos2d::Menu::create(pSCItemImage,NULL) ;\
+        cocos2d::Menu* pMenu = cocos2d::Menu::create(pSCItemImage,NULL);\
         pMenu->setContentSize(pSCTemSprite[0]->getContentSize());\
-        ___SC_INSIDE_ADD_LAYOUT_NODE(sc_container_pParentNode,pMenu,(_x_),(_y_)) ;\
+        ___SC_INSIDE_ADD_LAYOUT_NODE(sc_container_pParentNode,pMenu,(_x_),(_y_));\
         SCNodeProperty::setProperty<cocos2d::Menu>(pMenu,purifyProertyString((_property_)));\
     }\
     _buttonItem2InfoMap[pSCItemImage] = purifyBehavior(this,(_fun_),pSCItemImage);\
     assignNode(pSCItemImage,(_node_));\
     SCNodeProperty::setProperty<cocos2d::MenuItemSprite>(pSCItemImage,purifyProertyString((_property_)));\
-    sc_container_LayoutObjectList.push_back(pSCItemImage);\
-}while(0);
+    sc_container_LayoutObjectList.push_back(pSCItemImage);
+
+// button label
+#define ___SC_INSIDE_ADD_BUTTON_LABEL(_node_,_x_,_y_,_property_,_label_,_fun_)\
+    cocos2d::MenuItemLabel* pSCTemItemLabel = cocos2d::MenuItemLabel::create((_label_),\
+    [this](cocos2d::Ref* pSender) { this->onSCMenuItemPressed(pSender);}); \
+    if(!sc_container_MenuItemArray.empty()){\
+        sc_container_MenuItemArray.pushBack(pSCTemItemLabel);\
+    }else if(sc_container_pButtonListMenu!=NULL){\
+        sc_container_pButtonListMenu->addChild(pSCTemItemLabel);\
+    }else{\
+        cocos2d::Menu* pMenu = cocos2d::Menu::create(pSCTemItemLabel,NULL) ;\
+        ___SC_INSIDE_ADD_LAYOUT_NODE(sc_container_pParentNode,pMenu,(_x_),(_y_));\
+        SCNodeProperty::setProperty<cocos2d::Menu>(pMenu,purifyProertyString((_property_)));\
+    }\
+    _buttonItem2InfoMap[pSCTemItemLabel] = purifyBehavior(this,(_fun_),pSCTemItemLabel);\
+    assignNode(pSCTemItemLabel,(_node_));\
+    SCNodeProperty::setProperty<cocos2d::MenuItemLabel>(pSCTemItemLabel,purifyProertyString((_property_)));\
+    sc_container_LayoutObjectList.push_back(pSCTemItemLabel);
 
 
 #endif // __SPEEDCC__SCUIMACRO_H__
--- test/ui-test/Classes/HomeSceneController.cpp
+++ test/ui-test/Classes/HomeSceneController.cpp
@@ -9,13 +9,27 @@ void HomeSceneController::onCreate(SCDictionary parameters)
 {
     SCSceneControllerT::onCreate(parameters);
 
-
+    Sprite* pSprite = cocos2d::Sprite::create("HelloWorld.png");
 //    SCNodeProperty::setProperty<Sprite>(pSprite, "scale=2;");
 //    this->getRootLayer()->addChild(pSprite);
     SC_BEGIN_CONTAINER_ROOT(NULL,0,0,NULL,SCWinSize())
 //        SC_INSERT_USER_NODE(pSprite, 0, 0, "scale=2;")
 //        SC_INSERT_SPRITE(NULL,0,0,"scale=2;","HelloWorld.png")
-        SC_INSERT_BUTTON_IMAGE(NULL,0,0,NULL,"HelloWorld.png","HelloWorld.png","HelloWorld.png",FN(onButtonClicked))
+//        SC_INSERT_BUTTON_IMAGE(NULL,0,0,NULL,"HelloWorld.png","HelloWorld.png","HelloWorld.png",FN(onButtonClicked))
+//        SC_BEGIN_CONTAINER_BUTTON_IMAGE(NULL,0,0,NULL,"HelloWorld.png","HelloWorld.png","HelloWorld.png",FN(onButtonClicked))
+//            SC_INSERT_LABEL_TTF(NULL,0,0,"color-text=red;","ABCDEFG","fonts/Marker Felt.ttf",23)
+//        SC_END_CONTAINER
+//        SC_BEGIN_CONTAINER_SPRITE(NULL,0,0,"scale=2;","HelloWorld.png")
+//            SC_INSERT_LABEL_TTF(NULL,0,0,"color-text=red;","ABCDEFG","fonts/Marker Felt.ttf",23)
+    SC_INSERT_BUTTON_TTF(NULL,0,0,"color-text=red;","ABCDEFG","fonts/Marker Felt.ttf",23,FN(onButtonClicked))
+//        SC_END_CONTAINER
+    
+//        SC_BEGIN_CONTAINER_USER_NODE(pSprite, 0, 0, "scale=2;")
+//            SC_INSERT_LABEL_TTF(NULL,0,0,"color-text=red;","ABCDEFG","fonts/Marker Felt.ttf",23)
+//        SC_END_CONTAINER
+//        SC_BEGIN_CONTAINER_LABEL_TTF(NULL,0,0,"color-text=red;","ABCDEFG","fonts/Marker Felt.ttf",23)
+//            SC_INSERT_SPRITE(NULL,0,0,"scale=0.4;","HelloWorld.png")
+//        SC_END_CONTAINER
     SC_END_CONTAINER
 }
 
